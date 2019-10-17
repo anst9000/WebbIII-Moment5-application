191b53ed3bc1b53c15bdcdc96e57c0cd9f6baf02
Anders Stromberg <coltlaboy@yahoo.se>
2019-10-17 07:27:28 +0200

    splitting js files into two separate

---
+++ src/js/fetch.js
@@ -0,0 +1,123 @@
+const preUrl = 'http://studenter.miun.se/~anst9000/writeable/dt173g/api/course/';
+
+function submitUpdateForm() {
+  let id = $('#updateid').val();
+  let name = $('#updatename').val();
+  let code = $('#updatecode').val();
+  let progression = $('#updateprogression').val();
+  let syllabus = $('#updatesyllabus').val();
+
+  let data = {
+    'id': id,
+    'name': name,
+    'code': code,
+    'progression': progression,
+    'syllabus': syllabus
+  };
+
+  let url = preUrl + 'update.php';
+  let parcel = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
+
+  fetch(url, parcel)
+    .then(resp => resp.json())
+    .then(result => {
+      console.log(result);
+      $("#updateModal").modal('toggle');
+      $('.modal-body').html("");
+
+      $("#updateInfoModal").modal("toggle");
+      $('#updateInfoModalLabel').text("Det gick bra!");
+      $("#updateInfoModal .modal-body").html("<p><span>" + name + "</span></p><br>").append("<p>Kursen är nu uppdaterad</p>");
+
+      slowlyCloseModal();
+    }).catch(err => {
+      console.error("Something went wrong with update request. " + err);
+    });
+}
+
+function submitDeleteForm() {
+  let id = $('#deleteid').val();
+  let name = $('#deletename').val();
+  let data = {
+    'id': id
+  };
+
+  let url = preUrl + 'delete.php';
+  let parcel = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
+
+  fetch(url, parcel)
+    .then(resp => resp.json())
+    .then(result => {
+      console.log(result);
+      $('#deleteModal').modal('toggle');
+      $('.modal-body').html("");
+
+      $("#deleteInfoModal").modal("toggle");
+      $('#deleteInfoModalLabel').text("Det lyckades.");
+      $("#deleteInfoModal .modal-body").html("<p><span>" + name + "</span></p><br>").append("<p>Kursen är nu borttagen</p>");
+
+      slowlyCloseModal();
+    }).catch(err => {
+      console.error("Something went wrong with delete request. " + err);
+    });
+}
+
+function createCourse() {
+  let name = $('#createname').val();
+  let code = $('#createcode').val();
+  let progression = $('#createprogression').val();
+  let syllabus = $('#createsyllabus').val();
+
+  let data = {
+    "name": name,
+    "code": code,
+    "progression": progression,
+    "syllabus": syllabus
+  };
+  let url = preUrl + "create.php";
+
+  const parcel = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
+
+  fetch(url, parcel)
+    .then(resp => resp.json())
+    .then(result => {
+      console.log(result);
+      $(window).scrollTop(0);
+      $('.modal-body').html("");
+
+      $("#createInfoModal").modal("toggle");
+      $('#createInfoModalLabel').text("Det är klart!");
+      $("#createInfoModal .modal-body").html("<p><span>" + name + "</span></p><br>").append("<p>Kursen är tillagd.</p>");
+
+      slowlyCloseModal();
+    }).catch(err => {
+      console.error('There was a problem with the POST request: ', err.message);
+    });
+}
+
+function slowlyCloseModal() {
+  window.setTimeout(() => {
+    // removes the "active" class to .popup and .popup-content after 2,5 sec
+    $(window).scrollTop(0);
+    $("#deleteInfoModal").modal("toggle");
+    location.reload();
+  }, 2500);
+}
--- src/js/main.js
+++ src/js/main.js
@@ -8,135 +8,6 @@
  * *********************************************************************************/
 'use strict';
 
-const preUrl = 'http://studenter.miun.se/~anst9000/writeable/dt173g/api/course/';
-
-function submitUpdateForm() {
-  let id = $('#updateid').val();
-  let name = $('#updatename').val();
-  let code = $('#updatecode').val();
-  let progression = $('#updateprogression').val();
-  let syllabus = $('#updatesyllabus').val();
-
-  let data = {
-    'id': id,
-    'name': name,
-    'code': code,
-    'progression': progression,
-    'syllabus': syllabus
-  };
-
-  let url = preUrl + 'update.php';
-  let parcel = {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  };
-
-  fetch(url, parcel)
-    .then(resp => resp.json())
-    .then(result => {
-      console.log(result);
-      $("#updateModal").modal('toggle');
-      $('.modal-body').html("");
-
-      $("#updateInfoModal").modal("toggle");
-      $('#updateInfoModalLabel').text("Det gick bra!");
-      $("#updateInfoModal .modal-body").html("<p><span>" + name + "</span></p><br>").append("<p>Kursen är nu uppdaterad</p>");
-
-      slowlyCloseModal();
-    }).catch(err => {
-      console.error("Something went wrong with update request. " + err);
-    });
-}
-
-function submitDeleteForm() {
-  let id = $('#deleteid').val();
-  let name = $('#deletename').val();
-  let data = {
-    'id': id
-  };
-
-  let url = preUrl + 'delete.php';
-  let parcel = {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  };
-
-  fetch(url, parcel)
-    .then(resp => resp.json())
-    .then(result => {
-      $('#deleteModal').modal('toggle');
-      $('.modal-body').html("");
-
-      $("#deleteInfoModal").modal("toggle");
-      $('#deleteInfoModalLabel').text("Det lyckades.");
-      $("#deleteInfoModal .modal-body").html("<p><span>" + name + "</span></p><br>").append("<p>Kursen är nu borttagen</p>");
-
-      slowlyCloseModal();
-    }).catch(err => {
-      console.error("Something went wrong with delete request. " + err);
-    });
-}
-
-function createCourse() {
-  let data = {};
-  let name = $('#createname').val();
-  let code = $('#createcode').val();
-  let progression = $('#createprogression').val();
-  let syllabus = $('#createsyllabus').val();
-
-  data = {
-    "name": name,
-    "code": code,
-    "progression": progression,
-    "syllabus": syllabus
-  };
-  let url = preUrl + "create.php";
-
-  const parcel = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  };
-
-  console.log("data is " + JSON.stringify(data));
-  console.log('url is ' + url);
-  console.log('parcel is ' + JSON.stringify(parcel));
-
-  fetch(url, parcel)
-    .then(resp => resp.json())
-    .then(result => {
-      console.log(result);
-      $(window).scrollTop(0);
-      $('.modal-body').html("");
-
-      $("#createInfoModal").modal("toggle");
-      $('#createInfoModalLabel').text("Det är klart!");
-      $("#createInfoModal .modal-body").html("<p><span>" + name + "</span></p><br>").append("<p>Kursen är tillagd.</p>");
-
-      // slowlyCloseModal();
-      console.log(result);
-    }).catch(err => {
-      console.error('There was a problem the POST request: ', err.message);
-    });
-}
-
-function slowlyCloseModal() {
-  window.setTimeout(() => {
-    // removes the "active" class to .popup and .popup-content after 2,5 sec
-    $(window).scrollTop(0);
-    $("#deleteInfoModal").modal("toggle");
-    location.reload();
-  }, 2500);
-}
-
 $(document).ready(_ => {
   $('#dtBasicExample').DataTable();
 
@@ -149,8 +20,7 @@ $(document).ready(_ => {
   fetch(url)
     .then(resp => resp.json())
     .then(result => {
-      console.log(result.records);
-
+      console.log(result);
       fetchResult.push(result.records);
 
       let output = `
